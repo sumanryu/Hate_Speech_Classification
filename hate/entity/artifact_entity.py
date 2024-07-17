5421e1d4d4f9648fd39f0fa742af82a75224c890
sumanryu <sumanryu@gmail.com>
2024-07-18 02:54:48 +0530

    Data Transformation module

--- hate/entity/artifact_entity.py
+++ hate/entity/artifact_entity.py
@@ -5,3 +5,11 @@ from dataclasses import dataclass
 class DataIngestionArtifacts:
     imbalance_data_file_path: str
     raw_data_file_path: str
+
+
+# Data Transformation artifacts
+@dataclass
+class DataTransformationArtifacts:
+    transformed_data_path: str
+
+    
--- hate/pipeline/train_pipeline.py
+++ hate/pipeline/train_pipeline.py
@@ -2,14 +2,16 @@ import sys
 from hate.logger import logging
 from hate.exception import CustomException
 from hate.components.data_ingestion import DataIngestion
+from hate.components.data_transformation import DataTransformation
 
-from hate.entity.config_entity import DataIngestionConfig
-from hate.entity.artifact_entity import DataIngestionArtifacts
+from hate.entity.config_entity import (DataIngestionConfig, DataTransformationConfig)
+from hate.entity.artifact_entity import (DataIngestionArtifacts, DataTransformationArtifacts)
+
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        
+        self.data_transformation_config = DataTransformationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifacts:
         logging.info("Entered the start_data_ingestion method of TrainPipeline class")
@@ -25,6 +27,24 @@ class TrainPipeline:
 
         except Exception as e:
             raise CustomException(e, sys) from e
+        
+    def start_data_transformation(self, data_ingestion_artifacts = DataIngestionArtifacts) -> DataTransformationArtifacts:
+        logging.info("Entered the start_data_transformation method of TrainPipeline class")
+        
+        try:
+            data_transformation = DataTransformation(
+            data_ingestion_artifacts = data_ingestion_artifacts,
+            data_transformation_config= self.data_transformation_config
+        )
+
+            data_transformation_artifacts = data_transformation.initiate_data_transformation()
+
+            logging.info("Exited the start_data_transformation method of TrainPipeline class")
+            return data_transformation_artifacts
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
     
     def run_pipeline(self):
 
@@ -32,6 +52,9 @@ class TrainPipeline:
 
         try:
             data_ingestion_artifacts = self.start_data_ingestion()
+            data_transformation_artifacts = self.start_data_transformation(
+                data_ingestion_artifacts=data_ingestion_artifacts
+            )
 
             logging.info("Exited the run_pipeline method of TrainPipeline class") 
 
