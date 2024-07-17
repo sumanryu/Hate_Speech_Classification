e7ef86a899ff525ceba3901e287128c5a96fd6e9
sumanryu <sumanryu@gmail.com>
2024-07-17 20:14:56 +0530

    Data Ingestion Module

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
 from hate.logger import logging
+from hate.exception import CustomException
 
-logging.info("Hello")
+from hate.configuration.gcloud_syncer import GCloudSync
+
+obj = GCloudSync()
+
+obj.sync_folder_from_gcloud("hate_speech_classification_2024", 'dataset.zip', 'download/dataset.zip')
